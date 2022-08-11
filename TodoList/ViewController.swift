4d1e262e79927bdf5e80bec957c85f5eac210d41
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-08-11 22:56:00 +0900

    setup_calender

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -77,6 +77,42 @@ class ViewController: UIViewController {
             return Task(title: title, done: done)
         }
     }
+    
+    func setupCalender(){
+        self.calenderView.delegate = self
+        self.calenderView.dataSource = self
+        
+        calenderView.scope = .month  // 월간
+        calenderView.locale = Locale(identifier: "ko_KR")
+        
+        calenderView.scrollEnabled = true   // 가능
+        calenderView.scrollDirection = .horizontal //가로
+        
+        // 헤더 폰트 설정
+        calenderView.appearance.headerTitleFont = UIFont(name: "NotoSansKR-Medium", size: 16)
+
+        // Weekday 폰트 설정
+        calenderView.appearance.weekdayFont = UIFont(name: "NotoSansKR-Regular", size: 14)
+
+        // 각각의 일(날짜) 폰트 설정 (ex. 1 2 3 4 5 6 ...)
+        calenderView.appearance.titleFont = UIFont(name: "NotoSansKR-Regular", size: 14)
+        
+        // 헤더의 날짜 포맷 설정
+        calenderView.appearance.headerDateFormat = "YYYY년 MM월"
+
+        // 헤더의 폰트 색상 설정
+        calenderView.appearance.headerTitleColor = UIColor.link
+
+        // 헤더의 폰트 정렬 설정
+        // .center & .left & .justified & .natural & .right
+        calenderView.appearance.headerTitleAlignment = .left
+
+        // 헤더 높이 설정
+        calenderView.headerHeight = 45
+
+        // 헤더 양 옆(전달 & 다음 달) 글씨 투명도
+        calenderView.appearance.headerMinimumDissolvedAlpha = 0.0   // 0.0 = 안보이게 됩니다.
+    }
 }
 
 extension ViewController: UITableViewDataSource {
@@ -127,3 +163,8 @@ extension ViewController: UITableViewDelegate {
         self.tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
+
+extension ViewController: FSCalendarDelegate, FSCalendarDataSource {
+    
+}
+
